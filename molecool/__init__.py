11982f40aa7a87d778633c009326b66b39219ead
chohaa <chohnna@vt.edu>
2022-07-15 15:02:29 -0400

    organize molecool into modules and subpackage

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,17 @@
 """A Python package for analyzing and visualizing xyz files."""
 
 # Add imports here
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
+
+
+
 from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
 
 # Handle versioneer
 from ._version import get_versions
--- molecool/functions.py
+++ molecool/functions.py
@@ -29,8 +29,34 @@ import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
 
+"""
+Data used for the rest of the package.
+"""
 
-#matplotlib notebook
+atomic_weights = {
+    'H': 1.00784,
+    'C': 12.0107,
+    'N': 14.0067,
+    'O': 15.999,
+    'P': 30.973762,
+    'F': 18.998403,
+    'Cl': 35.453,
+    'Br': 79.904,
+}
+
+
+atom_colors = {
+    'H': 'white',
+    'C': '#D3D3D3',
+    'N': '#add8e6',
+    'O': 'red',
+    'P': '#FFA500',
+    'F': '#FFFFE0',
+    'Cl': '#98FB98',
+    'Br': '#F4A460',
+    'S': 'yellow'
+}
+
 
 """
 This module is for functions that perform measurements.
@@ -73,36 +99,6 @@ def calculate_angle(rA, rB, rC, degrees=False):
         return np.degrees(theta)
     else:
         return theta
-
-
-"""
-Data used for the rest of the package.
-"""
-
-atomic_weights = {
-    'H': 1.00784,
-    'C': 12.0107,
-    'N': 14.0067,
-    'O': 15.999,
-    'P': 30.973762,
-    'F': 18.998403,
-    'Cl': 35.453,
-    'Br': 79.904,
-}
-
-
-atom_colors = {
-    'H': 'white',
-    'C': '#D3D3D3',
-    'N': '#add8e6',
-    'O': 'red',
-    'P': '#FFA500',
-    'F': '#FFFFE0',
-    'Cl': '#98FB98',
-    'Br': '#F4A460',
-    'S': 'yellow'
-}
-
 
 
 """
